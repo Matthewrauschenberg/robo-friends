6dffbc21ad4d755150de930a1439450324fd036b
Matthewrauschenberg <113645591+Matthewrauschenberg@users.noreply.github.com>
2023-01-31 16:36:26 -0500

    replaced classes with hooks
    in App,js replaced the classes with hooks.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBox from '../components/SearchBox'
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App() {
+        const [robots, setRobots] = useState([])
+        const [searchfield, setSearchfield] = useState('')
 
-    componentDidMount() {
+
+    
+    useEffect(()=> {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response=> response.json())
-            .then(users => this.setState({ robots: users}));
+            .then(users => {setRobots(users)});
+    }, [])
+
+   const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
-
-    render() {
-        const { robots, searchfield} = this.state;
         const filteredRobots = robots.filter(robot =>{
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
@@ -34,7 +29,7 @@ class App extends Component {
             (
                 <div className='tc'>
                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
                             <CardList robots={filteredRobots} />
@@ -42,8 +37,7 @@ class App extends Component {
                     </Scroll>
                 </div>
             );
-    }
-}
+ }
     
 //https://jsonplaceholder.typicode.com/users
 export default App;
